303dfbb4916e24c1bdc90a2ebd65dd923ba5782d
Aaron Ford <aaron@aaronford.net>
2018-07-09 21:52:52 +0000

    completed steps 5-6 - added headers and created secret file

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,25 @@
 var request = require('request');
+var secrets = require('./secrets');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, callback) {
+  var options = {
+    //url: 'https://api.github.com/repos/' + repoOwner + repoName + '/contributors',
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+    'User-Agent': 'aaronfordnet',
+    Authorization: secrets.GITHUB_TOKEN
+    }
+  }
+
+  request(options, function(err, response, body) {
+    callback(err, body)
+  })
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  if(err) {
+    console.log("Error received:", err);
+  }
+  console.log("Result:", result);
+});
