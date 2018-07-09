bd744368cb89c8fcd07453ff2666e5bc2bd331bb
Aaron Ford <aaron@aaronford.net>
2018-07-09 23:14:53 +0000

    completed step 7 - Parse JSON string and output each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,6 @@ var secrets = require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, callback) {
   var options = {
-    //url: 'https://api.github.com/repos/' + repoOwner + repoName + '/contributors',
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
     'User-Agent': 'aaronfordnet',
@@ -11,8 +10,11 @@ function getRepoContributors(repoOwner, repoName, callback) {
     }
   }
 
+
   request(options, function(err, response, body) {
-    callback(err, body)
+    // Parse string received from Github into JSON, and pass it into callback function
+    var parsed = JSON.parse(body);
+    callback(err, parsed)
   })
 }
 
@@ -21,5 +23,8 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   if(err) {
     console.log("Error received:", err);
   }
-  console.log("Result:", result);
+  // Loop over each item in parsed data from Github
+  result.forEach(function(user) {
+    console.log(user.avatar_url);
+  })
 });
