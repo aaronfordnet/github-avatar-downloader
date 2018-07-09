86d64f94b435c11e01542b492d275832bbac7d00
Aaron Ford <aaron@aaronford.net>
2018-07-09 23:51:41 +0000

    connected DownloadImageByURL function to getRepoContributors callback function

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,11 @@
+// Request required modules
 var request = require('request');
 var fs = require('fs');
 var secrets = require('./secrets');
 
+// MAIN FUNCTION
 function getRepoContributors(repoOwner, repoName, callback) {
+  // Set options for request
   var options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
@@ -11,34 +14,36 @@ function getRepoContributors(repoOwner, repoName, callback) {
     }
   }
 
+  // Make request
   request(options, function(err, response, body) {
-    // Parse string received from Github into JSON, and pass it into callback function
+    // Parse requested string into JSON, and pass it into callback function's 'result' parameter
     var parsed = JSON.parse(body);
     callback(err, parsed)
   })
 }
 
-
+// CALL MAIN FUNCTION
 getRepoContributors("jquery", "jquery", function(err, result) {
   if(err) {
     console.log("Error received:", err);
   }
-  // Loop over each item in parsed data from Github
+  // Loop over each item in parsed data from Github, call downloadImageByURL function for each user
   result.forEach(function(user) {
-    console.log(user.avatar_url);
+    var login = user.login;
+    var url = user.avatar_url;
+    var path = `./avatars/${login}.jpg`;
+    downloadImageByURL(url, path, login)
   })
 });
 
-
-function downloadImageByURL(url, filePath) {
+// DOWNLOAD IMAGE FUNCTION
+function downloadImageByURL(url, filePath, userName) {
   request.get(url)
        .on('error', function (err) {
          throw err;
        })
        .on('response', function (response) {
-         console.log('Response Status Code: ', response.statusCode);
+         console.log('Downloading avatar image:', userName);
        })
        .pipe(fs.createWriteStream(filePath));
 }
-
-downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466%22,%20%22avatars/kvirani.jpg', './downloads/avatar.jpg')
